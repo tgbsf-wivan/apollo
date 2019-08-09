bb704b173410747929e5f7b7566c4f5ac8fbd88e
Wim Vandamme <wim.vandamme@tgbsf.com>
2019-08-10 00:16:50 +0200

    initial version

---
+++ VRDemo/Assets/Networking/Editor/NetworkingEditor.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Networking))]
+[CanEditMultipleObjects]
+public class NetworkingEditor : Editor
+{
+
+  SerializedProperty isHost;
+  SerializedProperty address;
+  SerializedProperty port;
+
+  void OnEnable()
+  {
+    isHost = serializedObject.FindProperty("host");
+    address = serializedObject.FindProperty("address");
+    port = serializedObject.FindProperty("port");
+  }
+
+  public override void OnInspectorGUI()
+  {
+    serializedObject.Update();
+    EditorGUILayout.PropertyField(isHost);
+    EditorGUILayout.PropertyField(address);
+    EditorGUILayout.PropertyField(port);
+    serializedObject.ApplyModifiedProperties();
+  }
+
+}
+
+
---
+++ VRDemo/Assets/Networking/Scripts/Networking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Networking : NetworkManager
+{
+
+  public bool host = false;
+  public string address = "localhost";
+  public short port = 7777;
+
+  public override void OnServerConnect(NetworkConnection conn)
+  {
+    Debug.Log("OnPlayerConnected");
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    this.networkAddress = address;
+    this.networkPort = port;
+    if (host)
+    {
+      Debug.Log("Starting Host");
+      this.StartHost();
+    }
+    else
+    {
+      Debug.Log("Starting Client");
+      this.StartClient();
+    }
+  }
+}
