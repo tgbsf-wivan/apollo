3770b9864681e57fa22ef19290f153bdba9821b1
Wim Vandamme <wim.vandamme@tgbsf.com>
2019-08-10 18:13:46 +0200

    add interactivity

---
+++ VRDemo/Assets/Interaction/Scripts/OpenAndCloseDoors.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class OpenAndCloseDoors : NetworkBehaviour
+{
+
+  public Transform LeftDoor = null;
+  public Transform RightDoor = null;
+
+  private bool are_open = false;
+
+  [Command]
+  void CmdToggleDoors()
+  {
+    if (are_open)
+    {
+      LeftDoor.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+      RightDoor.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+      are_open = false;
+    }
+    else
+    {
+      LeftDoor.localRotation = Quaternion.Euler(0.0f, -60.0f, 0.0f);
+      RightDoor.localRotation = Quaternion.Euler(0.0f, 60.0f, 0.0f);
+      are_open = true;
+    }
+  }
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.D))
+    {
+      CmdToggleDoors();
+    }
+  }
+
+}
