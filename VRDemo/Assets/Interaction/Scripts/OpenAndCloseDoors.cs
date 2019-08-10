89fc49dae883e40b7102e73b23dfbdc4b5f0db96
Wim Vandamme <wim.vandamme@tgbsf.com>
2019-08-10 18:58:24 +0200

    ongoing dev

--- VRDemo/Assets/Interaction/Scripts/OpenAndCloseDoors.cs
+++ VRDemo/Assets/Interaction/Scripts/OpenAndCloseDoors.cs
@@ -28,11 +28,16 @@ public class OpenAndCloseDoors : NetworkBehaviour
     }
   }
 
+  void Start()
+  {
+      
+  }
+
   void Update()
   {
     if (Input.GetKeyDown(KeyCode.D))
     {
-      CmdToggleDoors();
+      if (hasAuthority) CmdToggleDoors();
     }
   }
 
